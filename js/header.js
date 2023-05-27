55244d15156f4abd66734bd172e6c4b5205c9224
Amatir_iam <122685493+mrr2dev@users.noreply.github.com>
2023-05-27 15:43:05 +0700

    Update header.js

--- js/header.js
+++ js/header.js
@@ -46,7 +46,7 @@ let isMetamaskConnected;
 
 let selectedAccount;
 
-let web3Main = new Web3('https://rpc.ankr.com/bsc_testnet_chapel/');
+let web3Main = new Web3('https://rpc.ankr.com/bsc');
 
 // Create an instance of Notyf
 var notyf = new Notyf({
